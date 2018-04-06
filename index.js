cfb08e6b35634436eb2ee8314bfca2daafa89c00
Richard S <rscheiwe@gmail.com>
2018-04-06 02:57:46 +0000

    Done.

--- index.js
+++ index.js
@@ -1,7 +1,30 @@
 function dwarfRollCall(dwarves) {
+  let output = ""
+  for (let i = 0, l = dwarves.length; i < l; i++) {
+    output += `${i + 1}. ${dwarves[i]} `
+  }
+  return output
 }
 
+//function dwarfRollCall(array) {
+//  let output = []
+//  for (let i = 0; i < array.length) {
+//    output.push('${i + 1}. ${array[i]')
+//  }
+//  return output
+//}
+
+
+
+//
+
 function summonCaptainPlanet(planeteerCalls){
+  let output = []
+  for (let i = 0, l = planeteerCalls.length; i < l; i++) {
+    planeteerCalls[i] = planeteerCalls[i].toUpperCase() + '!'
+    output.push(planeteerCalls[i])
+  }
+  return output
 }
 
 function longPlaneteerCalls(words) {
